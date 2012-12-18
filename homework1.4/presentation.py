05c93e5e7c67eae593b87cec99e56146e785aa56
ymcagodme <ymcagodme@gmail.com>
2012-12-17 18:31:42 -0800

    Add the code for the Final Exam

--- homework1.4/presentation.py
+++ homework1.4/presentation.py
@@ -1,13 +1,13 @@
 p=[0.2, 0.2, 0.2, 0.2, 0.2]
 
-world=['red', 'red', 'green', 'red', 'green']
-measurements = ['red']
-motions = [1,1]
-pHit = 0.8
-pMiss = 0.2
-pExact = 0.8
-pOvershoot = 0.1
-pUndershoot = 0.1
+world=['g', 'g', 'r', 'g', 'r']
+measurements = ['r']
+motions = [1]
+pHit = 0.9
+pMiss = 0.1
+pExact = 1.0
+pOvershoot = 0.0
+pUndershoot = 0.0
 
 def sense(p, Z):
     q=[]
@@ -22,15 +22,28 @@ def sense(p, Z):
 def move(p, U):
     q = []
     for i in range(len(p)):
-        s = pExact * p[(i-U) % len(p)]
-        s = s + pOvershoot * p[(i-U-1) % len(p)]
-        s = s + pUndershoot * p[(i-U+1) % len(p)]
+        if i - U < 0:
+            s = 0.0
+        else:
+            s = pExact * p[(i-U)]
+        if i + U > len(p) - 1:
+            s += p[i]
+        #s = s + pOvershoot * p[(i-U-1) % len(p)]
+        #s = s + pUndershoot * p[(i-U+1) % len(p)]
         q.append(s)
     return q
 
 for k in range(len(measurements)):
     p = sense(p, measurements[k])
-    p = move(p, motions[k])
+    #p = move(p, motions[k])
     
 print p         
 
+prob = 0.0
+for i in range(len(p)):
+    try:
+        hit = ('r' == world[i + 1])
+        prob += p[i] * (hit * pHit + (1 - hit) * pMiss)
+    except IndexError:
+        prob += p[i] * pHit
+print prob
